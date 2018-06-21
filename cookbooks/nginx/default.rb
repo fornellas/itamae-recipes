8d107e6ff18625ed68f85cfa3caf6df3474e34c6
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2018-06-21 23:19:14 +0100

    Add nginx server_names_hash_bucket_size

--- cookbooks/nginx/default.rb
+++ cookbooks/nginx/default.rb
@@ -1,5 +1,12 @@
 package 'nginx'
 
+remote_file "/etc/nginx/conf.d/server_names_hash_bucket_size.conf" do
+	mode '644'
+	owner 'root'
+	group 'root'
+	notifies :restart, 'service[nginx]', :immediately
+end
+
 service "nginx" do
 	action :enable
 end
