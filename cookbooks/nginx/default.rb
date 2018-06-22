06566292d711fc736b581c12e889bd4640cac8f8
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2018-06-22 22:44:44 +0100

    Add HTTPS

--- cookbooks/nginx/default.rb
+++ cookbooks/nginx/default.rb
@@ -1,5 +1,8 @@
 package 'nginx'
 
+package 'python3-certbot-nginx'
+package 'certbot'
+
 remote_file "/etc/nginx/conf.d/server_names_hash_bucket_size.conf" do
 	mode '644'
 	owner 'root'
--- cookbooks/octoprint/default.rb
+++ cookbooks/octoprint/default.rb
@@ -1,15 +1,28 @@
 include_recipe "../group_add"
 
+##
+## Configuration
+##
+
 home_path = '/var/lib/octoprint'
 port = 5000
 git_repo_url = "https://github.com/foosel/OctoPrint.git"
 domain = 'octoprint.sigstop.co.uk'
+email = 'fabio.ornellas@gmail.com'
+
+##
+## SetUp
+##
 
 basedir_path = "#{home_path}/.octoprint"
 configfile_path = "#{basedir_path}/config.yaml"
 git_repo_path = "#{home_path}/OctoPrint"
 virtualenv_path = "#{home_path}/virtualenv"
 unix_chkpwd_wrapper_path = "#{home_path}/unix_chkpwd_wrapper.sh"
+
+##
+## Packages
+##
 
 package 'python-pip'
 package 'python-dev'
@@ -19,7 +32,12 @@ package 'git'
 package 'libyaml-dev'
 package 'build-essential'
 
+##
+## User/Group
+##
+
 group 'octoprint'
+
 user 'octoprint' do
 	gid 'octoprint'
 	home home_path
@@ -27,9 +45,14 @@ user 'octoprint' do
 	shell '/usr/sbin/nologin'
 	create_home true
 end
+
 group_add 'octoprint' do
 	groups ['tty', 'dialout']
 end
+
+##
+## Git
+##
 
 execute "git clone #{git_repo_url} #{git_repo_path}" do
 	user 'octoprint'
@@ -40,6 +63,10 @@ execute "git clone #{git_repo_url} #{git_repo_path}" do
 		}\\$
 	EOF
 end
+
+##
+## Install to VirtualEnv
+##
 
 execute "virtualenv #{virtualenv_path}" do
 	user 'octoprint'
@@ -53,9 +80,30 @@ execute "#{virtualenv_path}/bin/python setup.py install" do
 	not_if "#{virtualenv_path}/bin/pip list | grep -E '^OctoPrint +'"
 end
 
+##
+## Default config
+##
+
 execute "mkdir #{basedir_path}" do
 	user 'octoprint'
 	not_if "test -d #{basedir_path}"
+end
+
+remote_file configfile_path do
+	mode '644'
+	owner 'octoprint'
+	group 'octoprint'
+	not_if "test -e #{configfile_path}"
+end
+
+##
+## Sudo / PAM
+##
+
+remote_file unix_chkpwd_wrapper_path do
+	mode '755'
+	owner 'root'
+	group 'root'
 end
 
 template "/etc/sudoers.d/octoprint" do
@@ -67,18 +115,18 @@ template "/etc/sudoers.d/octoprint" do
 	)
 end
 
-remote_file configfile_path do
+template "/etc/pam.d/octoprint" do
 	mode '644'
-	owner 'octoprint'
-	group 'octoprint'
-	not_if "test -e #{configfile_path}"
-end
-
-remote_file '/etc/init.d/octoprint' do
-	mode '755'
 	owner 'root'
 	group 'root'
+	variables(
+		unix_chkpwd_wrapper_path: unix_chkpwd_wrapper_path,
+	)
 end
+
+##
+## Service
+##
 
 template "/etc/default/octoprint" do
 	mode '644'
@@ -94,28 +142,39 @@ template "/etc/default/octoprint" do
 	notifies :restart, 'service[octoprint]', :immediately
 end
 
-service "octoprint" do
-	action :enable
-end
-
-include_recipe "../nginx"
-
-remote_file unix_chkpwd_wrapper_path do
+remote_file '/etc/init.d/octoprint' do
 	mode '755'
 	owner 'root'
 	group 'root'
 end
 
+service "octoprint" do
+	action :enable
+end
+
+##
+## Nginx
+##
+
+include_recipe "../nginx"
+
 package "libnginx-mod-http-auth-pam"
 
-template "/etc/pam.d/octoprint" do
-	mode '644'
-	owner 'root'
-	group 'root'
-	variables(
-		unix_chkpwd_wrapper_path: unix_chkpwd_wrapper_path,
-	)
+letsencrypt_requirements = [
+	'/etc/letsencrypt/live/octoprint.sigstop.co.uk/fullchain.pem',
+	'/etc/letsencrypt/live/octoprint.sigstop.co.uk/privkey.pem',
+	'/etc/letsencrypt/options-ssl-nginx.conf',
+	'/etc/letsencrypt/ssl-dhparams.pem',
+]
+
+test_has_letsencrypt_files = "#{letsencrypt_requirements.map{|f| "test -f #{f}"}.join(' ; ')}"
+
+execute "/usr/bin/certbot certonly -d #{domain} --nginx -n -m #{email} --agree-tos" do
+	user 'root'
+	not_if test_has_letsencrypt_files
 end
+
+run_command(test_has_letsencrypt_files, user: 'root')
 
 template '/etc/nginx/sites-enabled/octoprint' do
 	mode '644'
