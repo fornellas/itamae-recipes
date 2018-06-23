006c267084fb004d4d6fc76ebeffbcd1b3b6bcea
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2018-06-23 17:18:28 +0100

    Add root alias

--- cookbooks/postfix/default.rb
+++ cookbooks/postfix/default.rb
@@ -1,3 +1,5 @@
+email = "fabio.ornellas@gmail.com"
+
 package 'postfix'
 
 template "/etc/postfix/main.cf" do
@@ -9,5 +11,21 @@ template "/etc/postfix/main.cf" do
 end
 
 service 'postfix'
+
+file "/etc/aliases" do
+	action :edit
+	block do |content|
+		newalias = "root: #{email}"
+		unless content.include?(newalias)
+			content.replace("#{content}\nnewalias")
+		end
+	end
+	notifies :run, 'execute[/usr/bin/newaliases]', :immediately
+end
+
+execute "/usr/bin/newaliases" do
+	user 'root'
+	action :nothing
+end
 
 package 'mailutils'
