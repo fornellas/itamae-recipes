dc1985eac5eae98a90144143c872a159af9bbe79
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2018-06-28 10:29:12 +0100

    Add letsencrypt

---
+++ cookbooks/letsencrypt/default.rb
@@ -0,0 +1,22 @@
+email = "fabio.ornellas@gmail.com"
+
+include_recipe "../nginx"
+
+package 'python3-certbot-nginx'
+package 'certbot'
+
+define :letsencrypt, domain: nil do
+	domain = if params[:domain]
+		params[:domain]
+	else
+		params[:name]
+	end
+	certificate_files = [
+		"/etc/letsencrypt/live/#{domain}/fullchain.pem",
+		"/etc/letsencrypt/live/#{domain}/privkey.pem",
+	]
+	execute "/usr/bin/certbot certonly -d #{domain} --nginx -n -m #{email} --agree-tos " do
+		user 'root'
+		not_if "#{certificate_files.map{|f| "test -f #{f}"}.join(' && ')}"
+	end
+end
