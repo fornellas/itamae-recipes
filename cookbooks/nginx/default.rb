9849b972a8e449722d5f3588ec96968f7386aa89
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2018-06-28 10:28:34 +0100

    Add config

--- cookbooks/nginx/default.rb
+++ cookbooks/nginx/default.rb
@@ -1,9 +1,6 @@
 package 'nginx'
 
-package 'python3-certbot-nginx'
-package 'certbot'
-
-remote_file "/etc/nginx/conf.d/server_names_hash_bucket_size.conf" do
+remote_file "/etc/nginx/nginx.conf" do
 	mode '644'
 	owner 'root'
 	group 'root'
