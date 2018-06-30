e9f94480d123ee1996113661a800d640a3206894
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2018-06-30 20:46:26 +0100

    Add opcache conf

--- cookbooks/php-fpm/default.rb
+++ cookbooks/php-fpm/default.rb
@@ -8,4 +8,12 @@ file "/etc/php/#{php_version}/fpm/pool.d/www.conf" do
 	action :delete
 end
 
+remote_file "/etc/php/#{php_version}/fpm/conf.d/11-custom-opcache.ini" do
+	mode '644'
+	owner 'root'
+	group 'root'
+	source "files/etc/php/fpm/conf.d/11-custom-opcache.ini"
+	notifies :restart, "service[php#{php_version}-fpm]", :immediately
+end
+
 service "php#{php_version}-fpm"
