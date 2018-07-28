edb6fd0c88cb23c1fc773d4478a97a3fa3b9466e
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2018-07-28 13:41:43 +0100

    Allow bind queries from any

--- cookbooks/bind/default.rb
+++ cookbooks/bind/default.rb
@@ -1,2 +1,12 @@
 package 'bind9'
+
+file "/etc/bind/named.conf.options" do
+	owner 'root'
+	group 'bind'
+	mode '644'
+	notifies :restart, 'service[bind9]', :immediately
+end
+
+service 'bind9'
+
 package 'bind9utils'
