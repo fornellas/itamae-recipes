af64716d64b58ad8cd318bd3bfbac2372962ee88
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2021-11-30 13:25:15 +0000

    Add iptables_exporter

--- cookbooks/golang/default.rb
+++ cookbooks/golang/default.rb
@@ -1,4 +1,5 @@
-gopath = "/opt/go"
+gopath = "/var/cache/go"
+gobin = "/opt/bin"
 
 package 'golang-go'
 
@@ -6,10 +7,10 @@ define(
   :golang_install_bin,
   package: nil,
 ) do
-	bin = params[:name]
-	package = params[:package]
-  execute "GOPATH=#{gopath} go get #{package} && GOPATH=#{gopath} go install #{package}" do
-  	user "root"
-  	not_if "test -e #{gopath}/bin/#{bin}"
+  bin = params[:name]
+  package = params[:package]
+  execute "GOPATH=#{gopath} GOBIN=#{gobin} go get #{package} && GOPATH=#{gopath} GOBIN=#{gobin} go install #{package}" do
+    user "root"
+    not_if "test -e #{gobin}/#{bin}"
   end
 end
---
+++ cookbooks/iptables_exporter/default.rb
@@ -0,0 +1,54 @@
+listen_port = "9455"
+
+include_recipe "../golang"
+include_recipe "../iptables"
+
+##
+## iptables_exporter
+##
+
+# User / Group
+
+group "iptables_exporter"
+
+user "iptables_exporter" do
+  gid "iptables_exporter"
+  system_user true
+  shell "/usr/sbin/nologin"
+  create_home true
+end
+
+# Install
+
+golang_install_bin "iptables_exporter" do
+  package "github.com/retailnext/iptables_exporter"
+end
+
+# iptables
+
+iptables_rule_drop_not_user "Drop not prometheus user to iptables_exporter" do
+  users ["prometheus"]
+  port listen_port
+end
+
+# Service
+
+template "/etc/systemd/system/iptables_exporter.service" do
+  mode "644"
+  owner "root"
+  group "root"
+  variables(
+    listen_address: "127.0.0.1:#{listen_port}",
+  )
+  notifies :run, "execute[systemctl daemon-reload]"
+end
+
+execute "systemctl daemon-reload" do
+  action :nothing
+  user "root"
+  notifies :restart, "service[iptables_exporter]"
+end
+
+service "iptables_exporter" do
+  action :enable
+end
