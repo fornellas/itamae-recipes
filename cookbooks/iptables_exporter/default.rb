838808bddefa8ded0b7a2cdd4a7a53a28ec2058e
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2021-11-30 20:53:10 +0000

    Fix query

--- cookbooks/iptables_exporter/default.rb
+++ cookbooks/iptables_exporter/default.rb
@@ -66,3 +66,12 @@ prometheus_file_sd "iptables_exporter" do
     },
   ]
 end
+
+prometheus_rules "odroid-iptables_exporter" do
+  alerting_rules [
+    {
+      alert: "OdroidIPTablesDrop",
+      expr: 'rate(iptables_rule_bytes_total{job="iptables_exporter",instance="odroid.local:'"#{listen_port}"'",rule=~".* -j DROP$"}[1m]) > 0',
+    },
+  ]
+end
