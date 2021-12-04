0aef801b05724274b5528b9d1e80f4e88b8a6a87
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2021-12-04 13:30:42 +0000

    Extract home alerts

--- roles/odroid/default.rb
+++ roles/odroid/default.rb
@@ -35,132 +35,14 @@ end
 
 include_recipe "../../cookbooks/base_system"
 include_recipe "../../cookbooks/base_server"
+include_recipe "../../cookbooks/no_auth_from_securetty"
 include_recipe "../../cookbooks/prometheus"
-
-# Brown
-
-prometheus_file_sd "brown_windows_exporter" do
-  targets [
-    {
-      hosts: ["192.168.0.221:9182"],
-      labels: {
-        instance: "brown.local:9182",
-        job: "windows_exporter",
-      },
-    },
-  ]
-end
-
-prometheus_file_sd "brown_node_exporter" do
-  targets [
-    {
-      hosts: ["192.168.0.221:9100"],
-      labels: {
-        instance: "brown.local:9100",
-        job: "node_exporter",
-      },
-    },
-  ]
-end
-
-# Odroid
-
-prometheus_file_sd "odroid_node_exporter" do
-  targets [
-    {
-      hosts: ["127.0.0.1:9100"],
-      labels: {
-        instance: "odroid.local:9100",
-        job: "node_exporter",
-      },
-    },
-  ]
-end
-
-prometheus_rules "odroid" do
-  alerting_rules [
-    {
-      alert: "OdroidDown",
-      expr: 'up{instance="odroid.local:9100"} < 1',
-    },
-  ]
-end
-
-# Office
-
-prometheus_file_sd "office_sensors" do
-  targets [
-    {
-      hosts: ["192.168.0.138:9090"],
-      labels: {
-        instance: "office_sensors.local:9090",
-        job: "sensor",
-      },
-    },
-  ]
-end
-
-prometheus_rules "office_sensors" do
-  alerting_rules [
-    {
-      alert: "OfficeSensorDown",
-      expr: 'up{instance="office_sensors.local:9090"} < 1',
-    },
-    {
-      alert: "OfficeLowTemp",
-      expr: 'temperature_celsius{room="office"} < 20',
-    },
-    {
-      alert: "OfficeHighCO2",
-      expr: 'co2_concentration_ppm{room="office"} > 1600',
-    },
-  ]
-end
-
-# Living Room
-
-prometheus_file_sd "living_room_sensors" do
-  targets [
-    {
-      hosts: ["192.168.0.124:9090"],
-      labels: {
-        instance: "living_room_sensors:9090",
-        job: "sensor",
-      },
-    },
-  ]
-end
-
-prometheus_rules "living_room_sensors" do
-  alerting_rules [
-    {
-      alert: "LivingRoomSensorDown",
-      expr: 'up{instance="living_room_sensors.local:9090"} < 1',
-    },
-    {
-      alert: "LivingRoomLowTemp",
-      expr: 'temperature_celsius{room="living room"} < 20',
-    },
-  ]
-end
-
-# Stogare
-
-prometheus_rules "storage_camera" do
-  alerting_rules [
-    {
-      alert: "StorageCameraDown",
-      expr: 'up{instance="http://192.168.0.171/"} < 1',
-    },
-  ]
-end
-
+include_recipe "home_alerts"
 include_recipe "../../cookbooks/node_exporter"
 include_recipe "../../cookbooks/blackbox_exporter"
 include_recipe "../../cookbooks/iptables_exporter"
 include_recipe "../../cookbooks/alertmanager"
 include_recipe "../../cookbooks/grafana"
-include_recipe "../../cookbooks/no_auth_from_securetty"
 include_recipe "../../cookbooks/cherrymusic"
 include_recipe "../../cookbooks/nextcloud"
 include_recipe "../../cookbooks/octoprint"
---
+++ roles/odroid/home_alerts.rb
@@ -0,0 +1,117 @@
+# Brown
+
+prometheus_file_sd "brown_windows_exporter" do
+  targets [
+    {
+      hosts: ["192.168.0.221:9182"],
+      labels: {
+        instance: "brown.local:9182",
+        job: "windows_exporter",
+      },
+    },
+  ]
+end
+
+prometheus_file_sd "brown_node_exporter" do
+  targets [
+    {
+      hosts: ["192.168.0.221:9100"],
+      labels: {
+        instance: "brown.local:9100",
+        job: "node_exporter",
+      },
+    },
+  ]
+end
+
+# Odroid
+
+prometheus_file_sd "odroid_node_exporter" do
+  targets [
+    {
+      hosts: ["127.0.0.1:9100"],
+      labels: {
+        instance: "odroid.local:9100",
+        job: "node_exporter",
+      },
+    },
+  ]
+end
+
+prometheus_rules "odroid" do
+  alerting_rules [
+    {
+      alert: "OdroidDown",
+      expr: 'up{instance="odroid.local:9100"} < 1',
+    },
+  ]
+end
+
+# Office
+
+prometheus_file_sd "office_sensors" do
+  targets [
+    {
+      hosts: ["192.168.0.138:9090"],
+      labels: {
+        instance: "office_sensors.local:9090",
+        job: "sensor",
+      },
+    },
+  ]
+end
+
+prometheus_rules "office_sensors" do
+  alerting_rules [
+    {
+      alert: "OfficeSensorDown",
+      expr: 'up{instance="office_sensors.local:9090"} < 1',
+    },
+    {
+      alert: "OfficeLowTemp",
+      expr: 'temperature_celsius{room="office"} < 20',
+    },
+    {
+      alert: "OfficeHighCO2",
+      expr: 'co2_concentration_ppm{room="office"} > 1600',
+    },
+  ]
+end
+
+# Living Room
+
+prometheus_file_sd "living_room_sensors" do
+  targets [
+    {
+      hosts: ["192.168.0.124:9090"],
+      labels: {
+        instance: "living_room_sensors:9090",
+        job: "sensor",
+      },
+    },
+  ]
+end
+
+prometheus_rules "living_room_sensors" do
+  alerting_rules [
+    {
+      alert: "LivingRoomSensorDown",
+      expr: 'up{instance="living_room_sensors.local:9090"} < 1',
+    },
+    {
+      alert: "LivingRoomLowTemp",
+      expr: 'temperature_celsius{room="living room"} < 20',
+    },
+  ]
+end
+
+# Stogare
+
+prometheus_rules "storage_camera" do
+  alerting_rules [
+    {
+      alert: "StorageCameraDown",
+      expr: 'up{instance="http://192.168.0.171/"} < 1',
+    },
+  ]
+end
