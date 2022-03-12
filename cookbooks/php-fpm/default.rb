3ac84296aaf8d18b17f4c3315eccaf8414503f54
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2022-03-12 16:07:29 +0000

    PHP-FPM define

--- cookbooks/php-fpm/default.rb
+++ cookbooks/php-fpm/default.rb
@@ -1,21 +1,28 @@
-php_major_version = "7"
-php_minor_version = "2"
-php_version = "#{php_major_version}.#{php_minor_version}"
+define(:php_fpm) do
+  php_version = params[:name]
 
-package "php#{php_version}-fpm"
+  package "php#{php_version}-fpm"
 
-file "/etc/php/#{php_version}/fpm/pool.d/www.conf" do
-  action :delete
-end
+  file "/etc/php/#{php_version}/fpm/pool.d/www.conf" do
+    action :delete
+  end
 
-remote_file "/etc/php/#{php_version}/fpm/conf.d/11-custom-opcache.ini" do
-  mode "644"
-  owner "root"
-  group "root"
-  source "files/etc/php/fpm/conf.d/11-custom-opcache.ini"
-  notifies :restart, "service[php#{php_version}-fpm]", :immediately
-end
+  file "/etc/php/#{php_version}/fpm/php.ini" do
+    action :edit
+    block do |content|
+      new_content = []
+      content.split("\n").each do |line|
+        if line.match(/^memory_limit /) then
+          new_content << "memory_limit = 256M"
+        else
+          new_content << line
+        end
+      end
+      content.replace(new_content.join("\n"))
+    end
+  end
 
-service "php#{php_version}-fpm" do
-  action [:enable, :start]
+  service "php#{php_version}-fpm" do
+    action [:enable, :start]
+  end
 end
