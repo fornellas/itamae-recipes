aad9f0a52786116b391bebf64d13b08a8855125d
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2022-04-17 19:23:14 +0100

    Fix iptables save

--- cookbooks/iptables/default.rb
+++ cookbooks/iptables/default.rb
@@ -1,3 +1,15 @@
+package "netfilter-persistent"
+package "iptables-persistent"
+
+service "netfilter-persistent" do
+  action [:enable, :start]
+end
+
+execute "netfilter-persistent save" do
+  action :nothing
+  user "root"
+end
+
 define(
   :iptables_rule,
   table: nil,
@@ -13,11 +25,6 @@ define(
     not_if "iptables -t #{table} -C #{rule}"
     notifies :run, "execute[netfilter-persistent save]", :immediately
   end
-
-  execute "netfilter-persistent save" do
-    action :nothing
-    user "root"
-  end
 end
 
 define(
@@ -31,10 +38,12 @@ define(
   iptables_rule "Accept output established and related" do
     table "filter"
     rule "OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT"
+    notifies :run, "execute[netfilter-persistent save]", :immediately
   end
 
   iptables_rule "Drop not #{users.join("|")} to 127.0.0.1:#{port}" do
     table "filter"
     rule "OUTPUT -d 127.0.0.1 -p tcp -m tcp --dport #{port} #{users.map { |user| "-m owner ! --uid-owner #{user}" }.join(" ")} -j DROP"
+    notifies :run, "execute[netfilter-persistent save]", :immediately
   end
 end
