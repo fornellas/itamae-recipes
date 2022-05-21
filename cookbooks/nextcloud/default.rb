f6b9c28e0b934acb43614dd2358f823fee93a043
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2022-05-21 22:43:38 +0100

    Change to IPv4

--- cookbooks/nextcloud/default.rb
+++ cookbooks/nextcloud/default.rb
@@ -235,7 +235,7 @@ occ = "#{php} #{install_path}/occ"
     user "nextcloud"
     command <<~EOF
       #{occ} config:system:set user_external 0 class --value="\\\\OCA\\\\UserExternal\\\\SSH"
-      #{occ} config:system:set user_external 0 arguments 0 --value localhost
+      #{occ} config:system:set user_external 0 arguments 0 --value 127.0.0.1
       #{occ} config:system:set user_external 0 arguments 1 --value 22
     EOF
     not_if "#{occ} config:system:get user_external"
