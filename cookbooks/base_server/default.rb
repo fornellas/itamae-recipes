413ecc979dd41f8570faf61f0561287bfe6c4bbf
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2022-05-21 22:43:22 +0100

    Add IPv6

--- cookbooks/base_server/default.rb
+++ cookbooks/base_server/default.rb
@@ -14,7 +14,7 @@ include_recipe "../../cookbooks/postfix"
 ##
 
 local_networks = run_command(
-  "/sbin/ip addr | /bin/grep -E ' inet ' | awk '{print $2}'",
+  "/sbin/ip addr | /bin/grep -E ' inet(|6) ' | awk '{print $2}'",
 ).stdout.split("\n").map do |line|
   address, mask = line.split("/")
   mask = "32" if not mask
