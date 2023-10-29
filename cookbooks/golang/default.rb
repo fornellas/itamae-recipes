c5f873d5d692e9fe7eeb693f6dd0d68fc986896a
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2023-10-29 11:31:58 +0000

    fix go install

--- cookbooks/golang/default.rb
+++ cookbooks/golang/default.rb
@@ -9,7 +9,7 @@ define(
 ) do
   bin = params[:name]
   package = params[:package]
-  execute "GOPATH=#{gopath} GOBIN=#{gobin} go get #{package} && GOPATH=#{gopath} GOBIN=#{gobin} go install #{package}" do
+  execute "GOPATH=#{gopath} GOBIN=#{gobin} go install #{package}" do
     user "root"
     not_if "test -e #{gobin}/#{bin}"
   end
