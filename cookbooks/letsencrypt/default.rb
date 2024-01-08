a5ea5aef06484b9e60a4b3263340fe4740d09be6
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2024-01-08 17:58:15 +0000

    add mdns-proxy

--- cookbooks/letsencrypt/default.rb
+++ cookbooks/letsencrypt/default.rb
@@ -16,10 +16,17 @@ define :letsencrypt, domain: nil do
     else
       params[:name]
     end
+  first_domain = domain.split(",").first
+  domain_file = first_domain
+  if domain_file.start_with? "*."
+    domain_file = domain_file.delete_prefix "*."
+  end
   certificate_files = [
-    "/etc/letsencrypt/live/#{domain}/fullchain.pem",
-    "/etc/letsencrypt/live/#{domain}/privkey.pem",
+    "/etc/letsencrypt/live/#{domain_file}/fullchain.pem",
+    "/etc/letsencrypt/live/#{domain_file}/privkey.pem",
   ]
+  # FIXME for wildcard certificates, this must be manually done:
+  # certbot certonly -d *.example.com -m $email --agree-tos --manual --preferred-challenges dns
   execute "/usr/bin/certbot certonly -d #{domain} --nginx -n -m #{email} --agree-tos " do
     user "root"
     not_if "#{certificate_files.map { |f| "test -f #{f}" }.join(" && ")}"
---
+++ cookbooks/mdns-proxy/default.rb
@@ -0,0 +1,106 @@
+node.validate! do
+  {
+    mdns_proxy: {
+      domain: string,
+      interface: string,
+      port: string,
+      service: string,
+      version: string,
+    },
+  }
+end
+
+
+domain = node[:mdns_proxy][:domain]
+interface = node[:mdns_proxy][:interface]
+port = node[:mdns_proxy][:port]
+listen_address_port = "127.0.0.1:#{port}"
+service = node[:mdns_proxy][:service]
+version = node[:mdns_proxy][:version]
+
+include_recipe "../golang"
+include_recipe "../iptables"
+include_recipe "../letsencrypt"
+include_recipe "../nginx"
+
+##
+## mDNS Proxy
+##
+
+  # User / Group
+
+    group "mdns-proxy"
+
+    user "mdns-proxy" do
+      gid "mdns-proxy"
+      system_user true
+      shell "/usr/sbin/nologin"
+    end
+
+  # iptables
+
+    iptables_rule_drop_not_user "Drop not www-data user to mDNS Proxy" do
+      users ["www-data"]
+      port port
+    end
+
+  # Install
+
+    golang_install_bin "mdns-proxy" do
+      package "github.com/fornellas/mdns-proxy@#{version}"
+    end
+
+  # Service
+
+    template "/etc/systemd/system/mdns-proxy.service" do
+      mode "644"
+      owner "root"
+      group "root"
+      variables(
+        address: listen_address_port,
+        base_domain: domain,
+        interface: interface,
+        service: service,
+      )
+      notifies :run, "execute[systemctl daemon-reload]"
+      notifies :restart, "service[mdns-proxy]"
+    end
+
+    execute "systemctl daemon-reload" do
+      action :nothing
+      user "root"
+      notifies :restart, "service[mdns-proxy]"
+    end
+
+    service "mdns-proxy" do
+      action [:enable, :start]
+    end
+
+##
+## Nginx
+##
+
+  # Certificate
+
+    letsencrypt "#{domain},*.#{domain}"
+
+  # Auth
+
+    remote_file "/etc/pam.d/mdns-proxy" do
+      mode "644"
+      owner "root"
+      group "root"
+    end
+
+  # Configuration
+
+    template "/etc/nginx/sites-enabled/mdns-proxy" do
+      mode "644"
+      owner "root"
+      group "root"
+      variables(
+        domain: domain,
+        mdns_proxy_port: port,
+      )
+      notifies :restart, "service[nginx]", :immediately
+    end
