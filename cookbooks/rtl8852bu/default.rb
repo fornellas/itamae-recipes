c471739e39916346b5b86dae850071865598b371
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2024-01-21 14:10:25 +0000

    fix package install

--- cookbooks/rtl8852bu/default.rb
+++ cookbooks/rtl8852bu/default.rb
@@ -2,7 +2,7 @@ package "dkms"
 
 execute "Install rtl8852bu-dkms.deb" do
   command "wget -O /tmp/rtl8852bu-dkms.deb https://linux.brostrend.com/rtl8852bu-dkms.deb && dpkg -i /tmp/rtl8852bu-dkms.deb && rm -f /tmp/rtl8852bu-dkms.deb"
-  not_if "dpkg -s grafana | grep -E '^Status: install ok installed$'"
+  not_if "dpkg -s rtl8852bu-dkms | grep -E '^Status: install ok installed$'"
 end
 
 execute "modprobe 8852bu" do
